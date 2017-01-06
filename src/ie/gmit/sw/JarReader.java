ecfe8a79151ff00e423cffaa6a1f304b6b53dbda
paulyd123 <g00297086@gmit.ie>
2017-01-06 14:09:03 +0000

    Added metric and metric calculator classes

--- src/ie/gmit/sw/JarReader.java
+++ src/ie/gmit/sw/JarReader.java
@@ -4,30 +4,54 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.jar.JarInputStream;
+import java.lang.reflect.*;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.*;
 import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
 
 
 public class JarReader {
+
+	List<Class> cls = new ArrayList<Class>();
+
+	public JarReader() {}
+
 	
-	private ArrayList<Class> jarContents = new ArrayList<Class>();
-	
-	//Empty constructor
-	public JarReader(){}
-	
-	//Retrieves the jar from the specified jar, put's each entry into an array
-	public void getJar(String jarName) throws FileNotFoundException, IOException{
-		JarInputStream in = new JarInputStream(new FileInputStream(new File(jarName)));
+	public void readJarFile(String jarFile) throws FileNotFoundException, IOException{
+
+		File file  = new File(jarFile);
+        URL url = file.toURI().toURL();
+        URL[] urls = new URL[]{url};
+
+        ClassLoader cl = new URLClassLoader(urls);
+		
+		JarInputStream in = new JarInputStream(new FileInputStream(new File(jarFile)));
 		JarEntry next = in.getNextJarEntry();
+
 		while (next != null) {
-		 if (next.getName().endsWith(".class")) {
-		 String name = next.getName().replaceAll("/", "\\.");
-		 name = name.replaceAll(".class", "");
-		 if (!name.contains("$")) name.substring(0, name.length() - ".class".length());
-		 System.out.println(name);
-		 }
-		 next = in.getNextJarEntry();
+			if (next.getName().endsWith(".class")) {
+				String name = next.getName().replaceAll("/", "\\.");
+				name = name.replaceAll(".class", "");
+				if (!name.contains("$")) name.substring(0, name.length() - ".class".length());
+				System.out.println(name);
+
+				Class queryClass;
+				try {
+					queryClass = Class.forName(name, false, cl);
+					cls.add(queryClass);
+					new Reflection(queryClass);
+					System.exit(0);
+				} 
+				catch (ClassNotFoundException e) {
+					System.out.println("Couldn't find class '" + name + "'");
+					//System.exit(0);
+				} 
+				 System.out.println(cls.size());
+			}
+			next = in.getNextJarEntry();
 		}
 		in.close();
 	}
--- src/ie/gmit/sw/Reflection.java
+++ src/ie/gmit/sw/Reflection.java
@@ -1,95 +1,91 @@
 package ie.gmit.sw;
 
 import java.lang.reflect.*;
+
+
 public class Reflection {
-   private Class c;
-   public static void main(String args[]){
-        if (args.length == 0) {
-            System.out.println("Enter class name:");
-            System.exit(1);
-        }
-        try {
-            Class queryClass = Class.forName(args[0]);
-            new Reflection(queryClass);
-        } catch (ClassNotFoundException ee) {
-            System.out.println("Unable to find class '"+ args[0] + "'");
-            System.exit(1);
-        }
-   }
 
-   public Reflection(Class c){
-      super();
-      this.c = c;
+	private Class cls;
 
-      printConstructors();
-      printFields();
-      printMethods();
-      createArray();
-   }
+	
+	public Reflection(Class cls){
+		super();
+		this.cls = cls;
+		getPackage();
+		getInterface();
+		getConstructor();
+		getFields();
+		getMethods();
+	}
 
-   public void printConstructors(){
-      Constructor ctorlist[] = c.getDeclaredConstructors();
-      System.out.println("--------------" + ctorlist.length + " Constructors --------------");
-      for (int i = 0; i < ctorlist.length; i++) {
-         Constructor ct = ctorlist[i];
-         System.out.println("\tname  = " + ct.getName());
-         System.out.println("\tdecl class = " + ct.getDeclaringClass());
+	private void getMethods() {
+		
+		//Gets set of methods
+		Method[] methods = cls.getMethods(); 
+		Class[] methodParams;
 
-         Class pvec[] = ct.getParameterTypes();
-         for (int j = 0; j < pvec.length; j++){
-            System.out.println("\tparam #" + j + " " + pvec[j]);
-         }
+		//Loop over the methods and print its name and its return type
+		for(Method m : methods){
 
-         Class evec[] = ct.getExceptionTypes();
-         for (int j = 0; j < evec.length; j++){
-            System.out.println("\texc #" + j + " " + evec[j]);
-         }
-         System.out.println("\t-----");
-      }
-   }
+			System.out.println("Method Name: " + m.getName());
+			//Gets method return type
+			Class methodReturnType = m.getReturnType(); 
+			System.out.println("Method Return Type: " + methodReturnType.getName());
 
-   public void printFields(){
-      Field fieldlist[] = c.getDeclaredFields();
-      for (int i = 0; i < fieldlist.length; i++) {
-         Field fld = fieldlist[i];
-         System.out.println("\tname = " + fld.getName());
-         System.out.println("\tdecl class = " + fld.getDeclaringClass());
-         System.out.println("\ttype = " + fld.getType());
-         int mod = fld.getModifiers();
-         System.out.println("\tmodifiers = " + Modifier.toString(mod));
-         System.out.println("-----");
-      }
-   }
+			//Gets method parameters
+			methodParams = m.getParameterTypes(); 
+			
+			//Loops over method parameters and prints out each method parameter name
+			for(Class mp : methodParams){
 
-   public void printMethods(){
-      Method methlist[] = c.getDeclaredMethods();
-      System.out.println("--------------" + methlist.length + " Methods --------------");
-      for (int i = 0; i < methlist.length;i++) {
-      	Method m = methlist[i];
-      	System.out.println("\tname = " + m.getName());
-      	System.out.println("\tdecl class = " + m.getDeclaringClass());
-      	Class pvec[] = m.getParameterTypes();
-      	for (int j = 0; j < pvec.length; j++){
-         		System.out.println("\tparam #" + j + " " + pvec[j]);
-    	}
-      	Class evec[] = m.getExceptionTypes();
-      	for (int j = 0; j < evec.length; j++){
-         		System.out.println("\texc #" + j + " " + evec[j]);
-      	}
-      	System.out.println("\treturn type = " + m.getReturnType());
-      	System.out.println("\t-----");
-      }
-   }
+				System.out.println("Method Parameter: " + mp.getName());
+			}
+		}
+	}
+	private void getFields() {
+		//Gets field and attributes
+		Field[] fields = cls.getFields(); 
+		
+		//Loops over fields and prints out field name
+		for(Field f : fields){
 
-   public void createArray(){
-      try {
-         Class cls = Class.forName("java.lang.String");
-         Object arr = Array.newInstance(cls, 10);
-         Array.set(arr, 5, "Msc OO");
-         String s = (String)Array.get(arr, 5);
-         System.out.println(s);
-      }catch (Throwable e) {
-         System.err.println(e);
-      }
-   }
+			System.out.println("Field Name: " + f.getName());
+		}
+	}
+	private void getConstructor() {
+		//Gets constructors
+		Constructor[] cons = cls.getConstructors(); 
+		Class[] constructorParams;
+
+		//Loops over constructor and prints out parameter name
+		for(Constructor c : cons){
+
+			System.out.println("Contructor Name: " + c.getName());
+			//Gets parameters
+			constructorParams = c.getParameterTypes(); 
+			for(Class param : constructorParams){
+
+				System.out.println("Constructor Parameter: " + param.getName());
+			}
+		}
+	}
+	private void getInterface() {
+		boolean iface = cls.isInterface();
+		System.out.println("Is this Class an Interface?: " + iface);
+
+		
+		Class[] interfaces = cls.getInterfaces(); 
+		
+		//Loops over the interface prints out interface name
+		for(Class i : interfaces){
+
+			System.out.println("Implements Interface: " + i.getName());
+		}
+	}
+	private void getPackage() {
+		
+		//Gets package
+		Package pack = cls.getPackage(); 
+		System.out.println("Package Name: " + pack.getName());
+	}
 }
